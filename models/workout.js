687ba4a5d66d64e3169d9225bf1673bcaea68f60
KB Desktop <Keithblack4290gmail.com>
2021-04-29 10:59:04 -0600

    put frontend js in own folder, created model

---
+++ models/workout.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema;
+
+new WorkoutSchema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "What type of exercise"
+            },
+
+            name: {
+                type: String,
+                trim:true,
+                required: "Enter a name for the workout"
+            },
+            duration: {
+                type: Number,
+                trim: true,
+                required: "How long was the workout"
+            },
+            distance: {
+                type: Number,
+            },
+            
+            weight:{
+                type: Number,
+            },
+            
+            reps: {
+                type: Number,
+            },
+
+            sets: {
+
+                type:Number
+            },
+            
+        },
+    ],
+});
+
+const Workout = mongoose.model("Workout",workoutSchema)
+
+module.exports = Workout;
